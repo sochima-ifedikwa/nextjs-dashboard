06074404a50251dcc36b47feabfcfee3cd0d3c9f
sochima-ifedikwa <ifedikwasochima@gmail.com>
2026-01-28 19:41:27 -0600

    chore: reorder import statements in auth.ts for consistency

--- auth.ts
+++ auth.ts
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth";
-import Credentials from "next-auth/providers/credentials";
 import { authConfig } from "./auth.config";
+import Credentials from "next-auth/providers/credentials";
 import { z } from "zod";
 import type { User } from "@/app/lib/definitions";
 import bcrypt from "bcrypt";
